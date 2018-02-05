c71de25107e2a500af9a7f6960b234b0daa4ed37
Ollasmyke <ollasmyke@gmail.com>
2018-02-05 15:21:43 +0100

    First Commit with VSC

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LoginService {
+
+  constructor(private http: Http) {}
+
+
+}
+
+  // public postAllValues(){
+  //   return this.http.post()
+
+  }
